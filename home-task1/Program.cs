ac09403602b450e7fb2c397068f5a32747606cc1
/dev/null <106831060+AlexeyDmitrich@users.noreply.github.com>
2022-12-09 15:36:52 +0300

    Update Program.cs
    1st task complete

--- home-task1/Program.cs
+++ home-task1/Program.cs
@@ -9,4 +9,14 @@
 
 int[] usersArray = InputArrInt();
 Print(usersArray);
+int res = PositiveNumsValue();
+Print(res);
 
+int PositiveNumsValue (int[] array){
+    int counter = 0;
+    for (int i = 0; i < array.length;){
+        if (array[i]>0) {
+            counter ++;
+        }
+    return counter;
+}
