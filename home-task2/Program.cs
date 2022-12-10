3429c9074d2e87a202a21ec5d376cf07db9df8b1
/dev/null <a.d.kamenev@yandex.ru>
2022-12-10 23:34:11 +0300

    2nd task complited

--- home-task2/Program.cs
+++ home-task2/Program.cs
@@ -1,22 +1,58 @@
-﻿/*
+﻿using static MyMethods.MyMethod;
+/*
 Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
 
-// предварительная формула:
-// x=(b2-b1)/(k1-k2) 
-// y=k1(b2-b1)/(k1-k2)+b1
+Print("Введите значения для линейного уравнения:");
+Print("  /‾");
+Print(" |  y = k1 * x + b1");
+Print("< ");
+Print(" |  y = k2 * x + b2");
+Print("  \\_");
 
-// if (k1 == k2){
-    // if (b1 == b2) {
-        // прямые совпадают
-    // } else {
-        // прямые параллельны
-    // }
-// } else {
-// здесь прямые поимеют точку пересечения, которую мы ищем.
+double k1 = DoubleInput("k1");
+double b1 = DoubleInput("b1");
+double k2 = DoubleInput("k2");
+double b2 = DoubleInput("b2");
+
+//InterPoint();
+
+// точка входа
+//void InterPoint (){
+    try {
+    double xCoordinate = FindVerticalPositionX(k1, b1, k2, b2);
+    double yCoordinate = FindVerticalPositionX(k1, b1, k2, b2);
+    Print($"Точка пересечения прямых находится на коордитнатах: ({xCoordinate}; {yCoordinate})");
+    } catch (Exception e) {
+        Print($"({e.Message})");
+        return;
+    }
+//}
 
 
-// }
+
+bool LinearFunctionIntersection (double k1, double b1, double k2, double b2){
+    bool haveIntersection = true;
+    if (k1 == k2){
+        if (b1 == b2) {
+            Print ("прямые совпадают");
+        } else {
+            Print ("прямые параллельны");
+        }
+        haveIntersection = false;
+    }
+    return haveIntersection;
+}
+
+double FindVerticalPositionX (double k1, double b1, double k2, double b2){
+    if (LinearFunctionIntersection(k1, b1, k2, b2)) return (b2-b1)/(k1-k2);
+    else throw new Exception("Отсутствует единая точка пересечения");
+}
+
+double FindHorisontalPositionY (double k1, double b1, double k2, double b2){
+    if (LinearFunctionIntersection(k1, b1, k2, b2)) return k1*(b2-b1)/(k1-k2)+b1;
+    else throw new Exception("Отсутствует единая точка пересечения");
+}
